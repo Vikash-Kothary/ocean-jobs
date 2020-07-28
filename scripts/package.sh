11dc90c4ffb4dab267b4b130d5379f89c979f443
Vikash Kothary <kothary.vikash@gmail.com>
2020-07-28 21:59:36 +0100

    scripts: Add placeholder scripts to build, release, package, deploy

---
+++ scripts/package.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+# file: package.sh
+# description: Package dist artifacts.
+
+echo 'TODO'
---
+++ scripts/release.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+# file: release.sh
+# description: Release Python package to PyPi repository.
+
+echo 'TODO'
