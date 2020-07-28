c31b07c375e04df77f7047b2aa90f072871ce574
Vikash Kothary <kothary.vikash@gmail.com>
2020-07-28 16:06:00 +0100

    Add test for lambda function

---
+++ tests/test_lambda_function.py
@@ -0,0 +1,18 @@
+
+import jsonpickle
+from aws_xray_sdk.core import xray_recorder
+
+from ocean_jobs.lambda_function import lambda_handler as handler
+
+def test_function():
+    xray_recorder.begin_segment('test_function')
+    with open('tests/event.json', 'rb') as file:
+        ba = bytearray(file.read())
+        event = jsonpickle.decode(ba)
+        print('## EVENT')
+        print(jsonpickle.encode(event))
+        context = {'requestid' : '1234'}
+        result = handler(event, context)
+        print(str(result))
+        assert 'FunctionCount' in str(result), 'Should match'
+    xray_recorder.end_segment()
