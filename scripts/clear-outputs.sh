cb46c80f6866063d2f3bc77089acad07765b3ea5
Vikash Kothary <kothary.vikash@gmail.com>
2020-07-29 21:21:13 +0100

    scripts: Ignore .ipynb_checkpoints when clearing jupyter outputs

--- scripts/clear-outputs.sh
+++ scripts/clear-outputs.sh
@@ -2,4 +2,4 @@
 # file: clear-outputs.sh
 # description: Clear outputs from jupyter notebooks.
 
-find notebooks -name  "*.ipynb" -print0 | xargs -0 nbstripout
+find notebooks -name  "*.ipynb" -not -path "*.ipynb_checkpoints/*" -print0 | xargs -0 nbstripout
