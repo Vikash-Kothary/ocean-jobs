38d77fb4b4a89d81bcb8b69a1514e0ff95cc0989
Vikash Kothary <kothary.vikash@gmail.com>
2020-07-29 22:25:29 +0100

    Add try catch if mock data function is unable account settings
    e.g. if it can't find the AWS Credentials

--- src/ocean_jobs/utils/mock.py
+++ src/ocean_jobs/utils/mock.py
@@ -2,6 +2,11 @@ import boto3
 
 
 def get_mock_data():
-    client = boto3.client("lambda")
-    response = client.get_account_settings()
+    try:
+        client = boto3.client("lambda")
+        response = client.get_account_settings()
+    except Exception as e:
+        print(e)
+    finally:
+        response = {"AccountUsage": {"FunctionCount": None}}
     return response["AccountUsage"]
